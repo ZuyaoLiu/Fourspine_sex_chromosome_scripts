fc918d8242e376f8bea6f0c733eab56406ab56ee
Zuyao Liu <49354864+ZuyaoLiu@users.noreply.github.com>
2022-03-18 16:49:45 +0100

    Update and rename barcode.list.sh to haplotaggingTo10X.sh

--- haplotaggingTo10X.sh
+++ haplotaggingTo10X.sh
@@ -3,4 +3,4 @@ read=$1
 zcat $read |grep "@"|sed -e "s/.*BX:Z://" -e "s/\sRX.*//"|sort -u > barcodes.list
 cat barcodes.list|paste - ../../haplotagging/4M-with-alts-february-2016.txt | awk 'NF == 2' > top5M.barcodes.GEM.list
 
-perl ../../haplotagging/10X_spoof.pl $read ""
+perl your_path/10X_spoof_from_Haplotagging.pl $read ""
